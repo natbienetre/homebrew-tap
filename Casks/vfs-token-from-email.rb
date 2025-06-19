a3ddc8c38bf29d51e3ebd2414281da7de86edff2
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2025-06-19 13:28:03 +0200

    feat(cask/vfs-token-from-email) Update to 20250619-111926
    Signed-off-by: Pierre PÉRONNET <pierre.peronnet@gmail.com>

--- Casks/vfs-token-from-email.rb
+++ Casks/vfs-token-from-email.rb
@@ -1,6 +1,6 @@
 cask "vfs-token-from-email" do
-    version "20250619-105427"
-    sha256 "867deb68f86b15bdb01acb8d9de52b2d4f7b999c1b7ac1abe859244530822b19"
+    version "20250619-111926"
+    sha256 "ead6584405f4e105ac37ceff8b33d5d5f3547e52bf9d37797b2007b91c63084a"
 
     url "https://github.com/natbienetre/wordpress-customer-data/releases/download/automator-workflow-#{version}/automator-workflow-#{version}.tar.gz"
     name "VFS Token from Email"
